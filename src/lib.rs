cff454e1dc950094f08386690f580709ddd045e8
901786 Cascino Romain <romain.cascino@gmail.com>
2018-11-12 22:55:24 +0700

    Add tests for search_links methods

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ impl Config {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Messages {
+pub struct JsonValue {
     messages: Vec<Message>,
 }
 
@@ -47,34 +47,108 @@ pub struct Share {
   link: String,
 }
 
-fn parse_messages(filename: String, site: Option<String>) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(filename)?;
-    let v: Messages = serde_json::from_str(&contents).unwrap();
+fn search_links_with_filter(messages: Vec<Message>, filter_site: String) -> Vec<String> {
+    let mut links = Vec::new();
+    for message in messages {
+      if message.share.is_some() {
+        let link = message.share.unwrap().link;
+        if link.contains(&filter_site) {
+          links.push(link);
+        }
+      }
+    }
+    links
+}
+
+fn search_links_without_filter(messages: Vec<Message>) -> Vec<String>{
+    let mut links = Vec::new();
+    for message in messages {
+      if message.share.is_some() {
+        links.push(message.share.unwrap().link);
+      }
+    }
+    links
+}
+
+fn parse_messages(json_value: JsonValue, site: Option<String>) -> Result<(), Box<dyn Error>> {
     if site.is_some() {
       let filter_site = site.unwrap();
-      for message in v.messages {
-        if message.share.is_some() {
-          let link = message.share.unwrap().link;
-          if link.contains(&filter_site) {
-            println!("{}", link);
-          }
-        }
-      }
+      search_links_with_filter(json_value.messages, filter_site);
     } else {
-      for message in v.messages {
-        if message.share.is_some() {
-          println!("{}", message.share.unwrap().link);
-        }
-      }
+      search_links_without_filter(json_value.messages);
     }
     Ok(())
 }
 
+fn parse_file(filename: String) -> Result<JsonValue, Box<dyn Error>> {
+    let contents = fs::read_to_string(filename)?;
+    let value: JsonValue = serde_json::from_str(&contents).unwrap();
+    Ok(value)
+}
+ 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    if let Err(e) = parse_messages(config.filename, config.site) {
+    let json_value = parse_file(config.filename).unwrap_or_else(|err| {
+        eprintln!("Problem parsing file: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = parse_messages(json_value, config.site) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_without_filter() {
+        let share1 = Share {
+          link: String::from("https://www.youtube.com/watch?v=aJUQO9l7k5s"),
+        };
+        let share2 = Share {
+          link: String::from("https://www.youtube.com/watch?v=dazedazed"),
+        };
+        let message1 = Message {
+          share: Some(share1),
+        };
+        let message2 = Message {
+          share: None,
+        };
+        let message3 = Message {
+          share: Some(share2),
+        };
+
+        assert_eq!(
+            vec![String::from("https://www.youtube.com/watch?v=aJUQO9l7k5s"), String::from("https://www.youtube.com/watch?v=dazedazed")],
+            search_links_without_filter(vec![message1, message2, message3])
+        );
+    }
+
+    #[test]
+    fn search_with_filter() {
+        let share1 = Share {
+          link: String::from("https://www.youtube.com/watch?v=aJUQO9l7k5s"),
+        };
+        let share2 = Share {
+          link: String::from("https://www.reddit.com/r/france"),
+        };
+        let message1 = Message {
+          share: Some(share1),
+        };
+        let message2 = Message {
+          share: None,
+        };
+        let message3 = Message {
+          share: Some(share2),
+        };
+
+        assert_eq!(
+            vec![String::from("https://www.reddit.com/r/france")],
+            search_links_with_filter(vec![message1, message2, message3], String::from("reddit"))
+        );
+    }
 }
